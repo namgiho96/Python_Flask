19e08abe65b46c0cd52f485bd65dad24732cabe7
namgiho96 <namgiho96@gmail.com>
2019-04-12 18:25:03 +0900

    2019-04-12 18:24

--- app.py
+++ app.py
@@ -8,6 +8,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/calc')
+def calc():
+    stmt = request.args.get('stmt', 'NONE')  # 3항처리
+    if stmt == 'NONE':
+        print('넘어온 값이 없이')
+    else:
+        print('넘어온 식 {}:'.format(stmt))
+        patt = '[0-9]+'
+        op =re.sub(patt, '', stmt)
+        print('넘어온 연산자 : {}'.format(op))
+        nums = stmt.split(op)
+
+        if op == '+':
+            result = int(nums[0]) + int(nums[1])
+        elif op == '-':
+            result = int(nums[0]) - int(nums[1])
+        elif op == '*':
+            result = int(nums[0]) * int(nums[1])
+        elif op == '/':
+            result = int(nums[0]) / int(nums[1])
+
+
+    return jsonify( result=result)
+
 
 if __name__ == '__main__':
     app.run()
